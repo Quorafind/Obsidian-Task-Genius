5b6dc175f4a997222bbb5ce0effde9a693403960
Adrien Romano <105045271+Adriz1er@users.noreply.github.com>
2025-05-01 11:26:46 +0200

    faire en sorte que les notice soient masquer automatiquement après 5 secondes

--- src/utils/RewardManager.ts
+++ src/utils/RewardManager.ts
@@ -183,7 +183,7 @@ export class RewardManager extends Component {
 		// Check if showRewardType is set to notice
 		if (this.settings.showRewardType === "notice") {
 			// Show a notice that automatically accepts the reward
-			new Notice(`🎉 ${reward.name}!`, 0);
+			new Notice(`🎉 ${reward.name}!`, 5000);
 			// Automatically accept the reward (decrease inventory)
 			this.acceptReward(reward);
 			return;
